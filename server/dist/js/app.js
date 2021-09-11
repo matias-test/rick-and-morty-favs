1d04ee8642724ebd8d7fc728714c038466fd374c
Matias Niklison <matias.niklison@copernic.tech>
2021-09-11 15:35:03 +0200

    [SERVER] Added Mongo Connection

--- server/dist/js/app.js
+++ server/dist/js/app.js
@@ -5,9 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const routes_1 = __importDefault(require("./routes"));
 const app = express_1.default();
-app.use(cors_1.default());
+app.use(cors_1.default({ origin: "http://localhost:3000" }));
 app.use(routes_1.default);
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+var mongoDB = process.env.REACT_APP_MONGO_URL;
+if (!mongoDB) {
+    throw new Error('Please configure the REACT_APP_MONGO_URL');
+}
+mongoose_1.default
+    .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+    console.log('Connected to mongo');
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+})
+    .catch(error => {
+    throw error;
+});
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,29 @@
 import express, { Express } from 'express';
 import cors from 'cors';
+import mongoose, { ConnectOptions } from "mongoose"
 import routes from './routes';
 
 const app: Express = express()
 
-app.use(cors())
+app.use(cors({ origin: "http://localhost:3000" }))
 app.use(routes);
 
 const PORT: string | number = process.env.PORT || 4000;
 
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+var mongoDB = process.env.REACT_APP_MONGO_URL;
+
+if (!mongoDB) {
+  throw new Error('Please configure the REACT_APP_MONGO_URL');
+}
+
+mongoose
+  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true } as ConnectOptions)
+  .then(() => {
+    console.log('Connected to mongo');
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    );
+  })
+  .catch(error => {
+    throw error;
+  })
